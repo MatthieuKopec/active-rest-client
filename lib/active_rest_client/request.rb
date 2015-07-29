5f3fc6c019d34d47e996e005ac38346fa8b3fbf6
MatthieuKopec <matkopec@gmail.com>
2015-07-29 10:18:12 +0200

    Update request.rb

--- lib/active_rest_client/request.rb
+++ lib/active_rest_client/request.rb
@@ -393,6 +393,7 @@ module ActiveRestClient
           end
         else
           if v.to_s[/\d{4}\-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(Z|[+-]\d{2}:\d{2})/]
+            Rails.logger.debug("****** RIK ET ROK ******")
             object._attributes[k] = v
           else
             object._attributes[k] = v

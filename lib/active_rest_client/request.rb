ffac697eaeff2fc3bfb6722a6bb80e999fe026ae
MatthieuKopec <matkopec@gmail.com>
2015-07-29 10:45:06 +0200

    Update request.rb

--- lib/active_rest_client/request.rb
+++ lib/active_rest_client/request.rb
@@ -394,7 +394,7 @@ module ActiveRestClient
         else
           Rails.logger.debug("****** RIK ET ROK ******")
           Rails.logger.debug(v)
-            object._attributes[k] = v
+            object._attributes[k] << v
         end
       end
       object.clean! unless object_is_class?
